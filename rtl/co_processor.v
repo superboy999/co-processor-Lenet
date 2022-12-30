c96775bf2c90c53851d26f59816c5a9b8facca24
superboy999 <chenwanqi99@163.com>
2022-12-30 15:46:29 +0800

    fix bug

--- rtl/co_processor.v
+++ rtl/co_processor.v
@@ -240,7 +240,7 @@ module e203_subsys_nice_core (
    );
    sirv_gnrl_dfflr #(1)   compute_result_dfflr (result_ready, result_digit_nxt, result_digit_r, nice_clk, nice_rst_n);
    assign result_ready_nxt = result_ready;
-   sirv_gnrl_dfflr #(1)   compute_result_dfflr (result_ready, result_ready_nxt, result_ready_r, nice_clk, nice_rst_n);
+   sirv_gnrl_dfflr #(1)   compute_ready_dfflr (result_ready, result_ready_nxt, result_ready_r, nice_clk, nice_rst_n);
    // nice_rsp_valid wait for nice_icb_rsp_valid in COMPUTE
    assign nice_rsp_valid_compute = state_is_compute & result_ready_r;
 
